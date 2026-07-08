3ae938bc296ca9a2fc359e561fe57a52f369a7dd
xenoz69-art <nekollyn1@gmail.com>
2026-07-08 13:51:23 +0700

    Update scrape.py

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,3 +1,23 @@
 from playwright.sync_api import sync_playwright
 
-print("Playwright berhasil diinstall!")
+URL = "https://sortitoutsi.net/best-football-manager-2021-wonderkids"
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    page.goto(URL, wait_until="networkidle", timeout=120000)
+
+    print("Title :", page.title())
+
+    page.screenshot(path="page.png", full_page=True)
+
+    html = page.content()
+
+    with open("page.html", "w", encoding="utf-8") as f:
+        f.write(html)
+
+    browser.close()
+
+print("Selesai")
